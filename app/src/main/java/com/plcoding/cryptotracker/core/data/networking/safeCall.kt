b8dce748f865d3b4821dc428c2a55b64073027e8
vs-machado <vinicius.s.machado@protonmail.com>
2025-03-21 14:21:36 -0300

    Added the network layer utility

---
+++ app/src/main/java/com/plcoding/cryptotracker/core/data/networking/safeCall.kt
@@ -0,0 +1,33 @@
+package com.plcoding.cryptotracker.core.data.networking
+
+import com.plcoding.cryptotracker.core.domain.util.NetworkError
+import com.plcoding.cryptotracker.core.domain.util.Result
+import io.ktor.client.statement.HttpResponse
+import io.ktor.util.network.UnresolvedAddressException
+import kotlinx.coroutines.ensureActive
+import kotlinx.serialization.SerializationException
+import kotlin.coroutines.coroutineContext
+
+/**
+ * Generic network call wrapper that handles common networking errors and returns a Result type.
+ *
+ * @param T The expected type of successful response
+ * @param execute Lambda function that performs the actual HTTP request
+ * @return Result<T, NetworkError> containing either the successful response or an error
+ */
+suspend inline fun <reified T> safeCall(
+    execute: () -> HttpResponse
+): Result<T, NetworkError> {
+    val response = try {
+        execute()
+    } catch(e: UnresolvedAddressException) {
+        return Result.Error(NetworkError.NO_INTERNET)
+    } catch (e: SerializationException) {
+        return Result.Error(NetworkError.SERIALIZATION)
+    } catch (e: Exception) {
+        coroutineContext.ensureActive()
+        return Result.Error(NetworkError.UNKNOWN)
+    }
+
+    return responseToResult(response)
+}
